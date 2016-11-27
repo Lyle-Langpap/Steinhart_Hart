dfb8ef04dc470faf708caa2cff4d28f348bbf2c3
Lyle Langpap <lyle.langpap@entiuum.com>
2016-11-27 18:30:17 -0400

    added comments to test

--- unittest/main-Steinhart_Hart_Test.c
+++ unittest/main-Steinhart_Hart_Test.c
@@ -4,12 +4,20 @@
 
 int main(int argc, char **argv)
 {
-	struct Steinhart_Hart_Coefficients coefficients = Get_Coefficients(25415, 5 + 273.15f, 10021, 25 + 273.15f, 6545, 35 + 273.15f);
+	// Get Steinhart-Hart coefficients based on known resistance/temperature pairs
+	// for an NTC thermistor.
+	struct Steinhart_Hart_Coefficients coefficients = Get_Coefficients(
+		25415, 5 + 273.15f, 
+		10021, 25 + 273.15f, 
+		6545, 35 + 273.15f);
 
+	// Compare the calculated coefficients with known coefficients.
 	Test_Range_Float("A", coefficients.a, 0.00113840f, 0.001f);
 	Test_Range_Float("B", coefficients.b, 0.00023245f, 0.0001f);
 	Test_Range_Float("C", coefficients.c, 0.00000009489f, 0.00000001f);
 
+	// Calculate the temperature at the resistances used to calculate the coefficients.
+	// The temperature should match within a small amount.
 	Test_Range_Float("T1 Kelvin", Get_Temperature_Kelvin(&coefficients, 25415), 5 + 273.15f, 0.1f);
 	Test_Range_Float("T2 Kelvin", Get_Temperature_Kelvin(&coefficients, 10021), 25 + 273.15f, 0.1f);
 	Test_Range_Float("T3 Kelvin", Get_Temperature_Kelvin(&coefficients, 6545), 35 + 273.15f, 0.1f);
